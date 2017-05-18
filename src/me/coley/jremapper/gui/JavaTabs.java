7a5955cfa52cb8881d682d022c9c8ebdc9d1dbf5
Col-E <mcoley192@icloud.com>
2017-05-18 19:13:57 -0400

    Cleaned up MainWindow class

---
+++ src/me/coley/jremapper/gui/JavaTabs.java
@@ -0,0 +1,126 @@
+package me.coley.jremapper.gui;
+
+import java.awt.Component;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.swing.JTabbedPane;
+
+import me.coley.jremapper.gui.component.JavaTextArea;
+
+@SuppressWarnings("serial")
+public class JavaTabs extends JTabbedPane {
+	/**
+	 * Map of tab titles to text areas containing the decompiled source.
+	 */
+	private Map<String, JavaTextArea> tabToSource = new HashMap<>();
+
+	/**
+	 * Adds a tab with the given title containing the given component <i>(of
+	 * type JavaTextArea)</i> to the tabbed pane.
+	 * 
+	 * @param title
+	 *            Title of tab to add.
+	 * @param component
+	 *            Component to fill the tab content. Must be a JavaTextArea.
+	 */
+	@Override
+	public void addTab(String title, Component component) {
+		if (component instanceof JavaTextArea) {
+			JavaTextArea text = (JavaTextArea) component;
+			int index = getTabCount();
+			tabToSource.put(title, text);
+			super.addTab(title, component);
+			setSelectedIndex(index);
+		} else {
+			throw new RuntimeException("Component was not a supported type!");
+		}
+	}
+
+	/**
+	 * Return the JavaTextArea associated with the given tab title.
+	 * 
+	 * @param title
+	 *            Title of a tab containing a JavaTextArea component.
+	 * @return JavaTextArea tab with the given title.
+	 */
+	public JavaTextArea getSourceArea(String title) {
+		return tabToSource.get(title);
+	}
+
+	/**
+	 * Removes a tab at the given index.
+	 * 
+	 * @param index
+	 */
+	@Override
+	public void remove(int index) {
+		String title = getTitleAt(index);
+		if (hasTab(title)) {
+			tabToSource.remove(title);
+		}
+		super.remove(index);
+	}
+
+	/**
+	 * Returns true if the tabbed pane has a tab of the given title open. False
+	 * otherwise.
+	 * 
+	 * @param title
+	 *            Title of a tab.
+	 * @return True if tab exists, false otherwise.
+	 */
+	public boolean hasTab(String title) {
+		return tabToSource.containsKey(title);
+	}
+
+	/**
+	 * Removes the tab by the given title.
+	 * 
+	 * @param title
+	 *            Title of the tab to remove.
+	 * @return True if removal successful, false otherwise.
+	 */
+	public boolean removeTab(String title) {
+		int index = getTitleIndex(title);
+		if (index == -1) {
+			return false;
+		}
+		remove(index);
+		return true;
+	}
+
+	/**
+	 * Selects the tab by the given title.
+	 * 
+	 * @param title
+	 *            Title of the tab to select.
+	 * @return True if selection successful, false otherwise.
+	 */
+	public boolean selectTab(String title) {
+		int index = getTitleIndex(title);
+		if (index == -1) {
+			return false;
+		}
+		setSelectedIndex(index);
+		return true;
+	}
+
+	/**
+	 * Finds the index of the tab by the given title.
+	 * 
+	 * @param title
+	 *            Title of the tab to search for.
+	 * @return Index of the tab.
+	 */
+	private int getTitleIndex(String title) {
+		int index = -1;
+		for (int i = 0; i < getTabCount(); i++) {
+			if (getTitleAt(i).equals(title)) {
+				index = i;
+				break;
+			}
+		}
+		return index;
+	}
+}
--- src/me/coley/jremapper/gui/MainWindow.java
+++ src/me/coley/jremapper/gui/MainWindow.java
@@ -2,7 +2,6 @@ package me.coley.jremapper.gui;
 
 import javax.swing.JCheckBox;
 import javax.swing.JFrame;
-import javax.swing.JTabbedPane;
 import javax.swing.JTextArea;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
@@ -10,22 +9,16 @@ import javax.swing.event.ChangeListener;
 import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
-import java.util.HashMap;
-import java.util.Map;
-
 import me.coley.bmf.mapping.ClassMapping;
 import me.coley.jremapper.CFRSetting;
 import me.coley.jremapper.Options;
 import me.coley.jremapper.Program;
 import me.coley.jremapper.History.RenameAction;
+import me.coley.jremapper.gui.action.*;
 import me.coley.jremapper.gui.component.JavaTextArea;
 import me.coley.jremapper.gui.component.NSplitPane;
 import me.coley.jremapper.gui.component.SearchPanel;
 import me.coley.jremapper.gui.component.tree.FileTree;
-import me.coley.jremapper.gui.listener.*;
-
 import javax.swing.JMenuBar;
 import javax.swing.JMenu;
 import javax.swing.JMenuItem;
@@ -35,10 +28,8 @@ public class MainWindow {
 	private final JFrame frame = new JFrame();
 	private FileTree pnlFileTree;
 	private SearchPanel pnlSearch;
-	private JTabbedPane pnlTabbedSources;
-	private Map<String, JavaTextArea> tabToSource = new HashMap<>();
+	private JavaTabs pnlJavaTabs;
 	private JavaTextArea currentSource;
-
 
 	/**
 	 * Create the application.
@@ -51,228 +42,223 @@ public class MainWindow {
 	 * Initialize the contents of the frame.
 	 */
 	public void initialize() {
-		// Setting up the main 3-way view
-		setupSplitView();
-		// Setting up the menu
+		// Creating the menu bar
 		setupMenu();
-	}
-
-	private void setupSplitView() {
+		// Creating the components to be added to the frame.
+		setupFrame();
+		setupFileTree();
+		setupSearchPanel();
+		setupSourceTabs();
+		// Create the splitpane that will contain the components
 		NSplitPane nsplit = new NSplitPane(3);
-		// Setting up the frame
-		{
-			// spMain.setDividerLocation(200);
-			frame.setTitle("JRemapper");
-			frame.setBounds(100, 100, 920, 578);
-			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-			frame.getContentPane().add(nsplit, BorderLayout.CENTER);
-		}
-		// Setting up the file tree for jar layouts
-		pnlFileTree = new FileTree(callback);
-		{
-			nsplit.addNComponent(0, 200, pnlFileTree);
-			// spMain.setLeftComponent(fileTree);
-		}
-		// Setting up the decompile area
-		// Tabbed panel will have tabs containing JavaTextAreas.
-		pnlTabbedSources = new JTabbedPane();
-		{
-			// Keep a history of which classes were opened.
-			pnlTabbedSources.addChangeListener(new ChangeListener() {
-				@Override
-				public void stateChanged(ChangeEvent e) {
-					int i = pnlTabbedSources.getSelectedIndex();
-					if (i < 0) {
-						return;
-					}
-					String title = pnlTabbedSources.getTitleAt(i);
-					callback.getHistory().onSelectClass(title);
-				}
-			});
-
-			// Add ability to middle-click tabs to close them.
-			pnlTabbedSources.addMouseListener(new MouseAdapter() {
-				@Override
-				public void mouseClicked(MouseEvent e) {
-					if (e.getButton() == MouseEvent.BUTTON2) {
-						int i = pnlTabbedSources.getSelectedIndex();
-						if (i < 0) {
-							return;
-						}
-						String title = pnlTabbedSources.getTitleAt(i);
-						if (tabToSource.containsKey(title)) {
-							tabToSource.remove(title);
-						}
-						pnlTabbedSources.remove(i);
-					}
-				}
-			});
-			nsplit.addNComponent(1, 600, pnlTabbedSources);
-			// spMain.setRightComponent(tabbedClasses);
-		}
-		// Set up the search results panel
-		pnlSearch = new SearchPanel(callback);
-		{
-			nsplit.addNComponent(2, 200, pnlSearch);
-		}
-		// Finish split-pane porportions
-		nsplit.setNDivider(0, 150);
-		nsplit.setNDivider(1, 600);
+		setupSplitView(nsplit);
 	}
 
 	private void setupMenu() {
 		JMenuBar menuBar = new JMenuBar();
-		{
-			frame.setJMenuBar(menuBar);
-			JMenu mnFile = new JMenu("File");
-			{
-				JMenu mnOpen = new JMenu("Open");
-				{
-					JMenuItem mntmOpenJar = new JMenuItem("Jar");
-					JMenuItem mntmOpenMap = new JMenuItem("Mappings");
-					mntmOpenJar.addActionListener(new ActionChooseFile(callback));
-					mntmOpenMap.addActionListener(new ActionLoadMapping(callback));
-					mnOpen.add(mntmOpenJar);
-					mnOpen.add(mntmOpenMap);
-				}
-				JMenu mnSave = new JMenu("Save As");
-				{
-					JMenuItem mntmSaveJar = new JMenuItem("Jar");
-					JMenuItem mntmSaveMap = new JMenuItem("Mapping");
-					mntmSaveMap.addActionListener(new ActionSaveAsMapping(callback));
-					mntmSaveJar.addActionListener(new ActionSaveAsJar(callback));
-					mnSave.add(mntmSaveJar);
-					mnSave.add(mntmSaveMap);
-				}
-				mnFile.add(mnOpen);
-				mnFile.add(mnSave);
-			}
-			JMenu mnOptions = new JMenu("Options");
-			{
-				for (final String setting : callback.getOptions().getOptions().keySet()) {
-					boolean enabled = callback.getOptions().get(setting);
-					final JCheckBox chk = new JCheckBox(setting);
-					chk.setSelected(enabled);
-					chk.addActionListener(new ActionListener() {
+		setupFileMenu(menuBar);
+		setupOptionsMenu(menuBar);
+		setupCFRMenu(menuBar);
+		setupMappingMenu(menuBar);
+		setupHistoryMenu(menuBar);
+		frame.setJMenuBar(menuBar);
+	}
+
+	private void setupHistoryMenu(JMenuBar menuBar) {
+		JMenu mnHistory = new JMenu("History");
+		// Create menu for opening/selecting the last few decompiled classes.
+		JMenu mnHistSelection = new JMenu("Selections");
+		callback.getHistory().registerSelectionUpdate(new Runnable() {
+			@Override
+			public void run() {
+				mnHistSelection.removeAll();
+				for (String title : callback.getHistory().getSelectedClasses()) {
+					JMenuItem mntmSel = new JMenuItem(title);
+					mntmSel.addActionListener(new ActionListener() {
 						@Override
 						public void actionPerformed(ActionEvent e) {
-							callback.getOptions().set(setting, chk.isSelected());
+							boolean opened = selectTab(title);
+							if (!opened) {
+								// TODO: Get class by looking up
+								// renames
+								// This won't open renamed classes
+								// right now.
+								ClassMapping mc = callback.getJarReader().getMapping().getMapping(title);
+								if (mc != null) {
+									callback.onClassSelect(mc);
+								}
+							}
 						}
 					});
-					mnOptions.add(chk);
+					mnHistSelection.add(mntmSel);
 				}
 			}
-			JMenu mnCfr = new JMenu("CFR");
-			{
-				for (final CFRSetting setting : CFRSetting.values()) {
-					final JCheckBox chk = new JCheckBox(setting.getText());
-					chk.setSelected(setting.isEnabled());
-					chk.addActionListener(new ActionListener() {
+
+		});
+		mnHistory.add(mnHistSelection);
+		// Create menu for undoing the last couple rename actions.
+		JMenu mnHistRename = new JMenu("Renames");
+		callback.getHistory().registerSelectionUpdate(new Runnable() {
+			@Override
+			public void run() {
+				mnHistRename.removeAll();
+				for (RenameAction rename : callback.getHistory().getRenameActions()) {
+					String title = rename.getBefore() + " -> " + rename.getAfter();
+					JMenuItem mntmRen = new JMenuItem(title);
+
+					mntmRen.addActionListener(new ActionListener() {
 						@Override
 						public void actionPerformed(ActionEvent e) {
-							setting.setEnabled(chk.isSelected());
-						}
-					});
-					mnCfr.add(chk);
-				}
-			}
-			JMenu mnMapping = new JMenu("Mapping");
-			{
-				JMenu mnMapCurrent = new JMenu("Current Class");
-				{
-					JMenuItem mntmRenameUnique = new JMenuItem("Rename-all uniquely");
-					JMenuItem mntmResetMembers = new JMenuItem("Reset members");
-					mntmRenameUnique.addActionListener(new ActionRenameCurrentMembers(callback));
-					mntmResetMembers.addActionListener(new ActionRenameReset(callback));
-					mnMapCurrent.add(mntmRenameUnique);
-					mnMapCurrent.add(mntmResetMembers);
-					mnMapping.add(mnMapCurrent);
-				}
-				JMenu mnMapAll = new JMenu("All Classes");
-				{
-					JMenuItem mntmRenameUnique = new JMenuItem("Rename-all: Simple");
-					mntmRenameUnique.addActionListener(new ActionRenameClasses(callback));
-					mnMapAll.add(mntmRenameUnique);
-					mnMapping.add(mnMapAll);
-				}
-			}
-			JMenu mnHistory = new JMenu("History");
-			{
-				JMenu mnHistSelection = new JMenu("Selections");
-				{
-					callback.getHistory().registerSelectionUpdate(new Runnable() {
-						@Override
-						public void run() {
-							mnHistSelection.removeAll();
-							for (String title : callback.getHistory().getSelectedClasses()) {
-								JMenuItem mntmSel = new JMenuItem(title);
-								mntmSel.addActionListener(new ActionListener() {
-									@Override
-									public void actionPerformed(ActionEvent e) {
-										boolean opened = selectTab(title);
-										if (!opened) {
-											// TODO: Get class by looking up
-											// renames
-											// This won't open renamed classes
-											// right now.
-											ClassMapping mc = callback.getJarReader().getMapping().getMapping(title);
-											if (mc != null) {
-												callback.onClassSelect(mc);
-											}
-										}
-									}
-								});
-								mnHistSelection.add(mntmSel);
+							rename.getMapping().name.setValue(rename.getBefore());
+							/*
+							 * if (rename.getMapping() instanceof ClassMapping)
+							 * { callback.updateTreePath(rename.
+							 * getMapping().name.original, rename.getBefore());
+							 * }
+							 */
+							callback.getHistory().onUndo(rename);
+							// TODO: This is a lazy fix, only the
+							// single class should need to be moved.
+							if (rename.getMapping() instanceof ClassMapping) {
+								callback.refreshTree();
 							}
 						}
-
 					});
-					mnHistory.add(mnHistSelection);
-				}
-				JMenu mnHistRename = new JMenu("Renames");
-				{
-					callback.getHistory().registerSelectionUpdate(new Runnable() {
-						@Override
-						public void run() {
-							mnHistRename.removeAll();
-							for (RenameAction rename : callback.getHistory().getRenameActions()) {
-								String title = rename.getBefore() + " -> " + rename.getAfter();
-								JMenuItem mntmRen = new JMenuItem(title);
 
-								mntmRen.addActionListener(new ActionListener() {
-									@Override
-									public void actionPerformed(ActionEvent e) {
-										rename.getMapping().name.setValue(rename.getBefore());
-										/*
-										 * if (rename.getMapping() instanceof
-										 * ClassMapping) {
-										 * callback.updateTreePath(rename.
-										 * getMapping().name.original,
-										 * rename.getBefore()); }
-										 */
-										callback.getHistory().onUndo(rename);
-										// TODO: This is a lazy fix, only the
-										// single class should need to be moved.
-										if (rename.getMapping() instanceof ClassMapping) {
-											callback.refreshTree();
-										}
-									}
-								});
-
-								mnHistRename.add(mntmRen);
-							}
-						}
-
-					});
-					mnHistory.add(mnHistRename);
+					mnHistRename.add(mntmRen);
 				}
 			}
-			menuBar.add(mnFile);
-			menuBar.add(mnOptions);
-			menuBar.add(mnCfr);
-			menuBar.add(mnMapping);
-			menuBar.add(mnHistory);
+
+		});
+		mnHistory.add(mnHistRename);
+		// Add to menu bar.
+		menuBar.add(mnHistory);
+	}
+
+	private void setupMappingMenu(JMenuBar menuBar) {
+		JMenu mnMapping = new JMenu("Mapping");
+		// Automated tasks for the current class
+		JMenu mnMapCurrent = new JMenu("Current Class");
+		JMenuItem mntmRenameUnique = new JMenuItem("Rename-all uniquely");
+		JMenuItem mntmResetMembers = new JMenuItem("Reset members");
+		mntmRenameUnique.addActionListener(new ActionRenameCurrentMembers(callback));
+		mntmResetMembers.addActionListener(new ActionRenameReset(callback));
+		mnMapCurrent.add(mntmRenameUnique);
+		mnMapCurrent.add(mntmResetMembers);
+		mnMapping.add(mnMapCurrent);
+		// Automated tasks for all loaded classes
+		JMenu mnMapAll = new JMenu("All Classes");
+		JMenuItem mntmRenameUniqueAll = new JMenuItem("Rename-all: Simple");
+		mntmRenameUniqueAll.addActionListener(new ActionRenameClasses(callback));
+		mnMapAll.add(mntmRenameUniqueAll);
+		mnMapping.add(mnMapAll);
+		// Add to menu bar.
+		menuBar.add(mnMapping);
+	}
+
+	private void setupFileMenu(JMenuBar menuBar) {
+		JMenu mnFile = new JMenu("File");
+		// Open options
+		JMenu mnOpen = new JMenu("Open");
+		JMenuItem mntmOpenJar = new JMenuItem("Jar");
+		JMenuItem mntmOpenMap = new JMenuItem("Mappings");
+		mntmOpenJar.addActionListener(new ActionChooseFile(callback));
+		mntmOpenMap.addActionListener(new ActionLoadMapping(callback));
+		mnOpen.add(mntmOpenJar);
+		mnOpen.add(mntmOpenMap);
+		// Save options
+		JMenu mnSave = new JMenu("Save As");
+		JMenuItem mntmSaveJar = new JMenuItem("Jar");
+		JMenuItem mntmSaveMap = new JMenuItem("Mapping");
+		mntmSaveMap.addActionListener(new ActionSaveAsMapping(callback));
+		mntmSaveJar.addActionListener(new ActionSaveAsJar(callback));
+		mnSave.add(mntmSaveJar);
+		mnSave.add(mntmSaveMap);
+		// Add to menu bar.
+		mnFile.add(mnOpen);
+		mnFile.add(mnSave);
+		menuBar.add(mnFile);
+	}
+
+	private void setupOptionsMenu(JMenuBar menuBar) {
+		JMenu mnOptions = new JMenu("Options");
+		// Iterate program options, create a checkbox for each.
+		// TODO: Options for non-boolean options.
+		for (final String setting : callback.getOptions().getOptions().keySet()) {
+			boolean enabled = callback.getOptions().get(setting);
+			final JCheckBox chk = new JCheckBox(setting);
+			chk.setSelected(enabled);
+			chk.addActionListener(new ActionListener() {
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					callback.getOptions().set(setting, chk.isSelected());
+				}
+			});
+			mnOptions.add(chk);
 		}
+		// Add to menu bar.
+		menuBar.add(mnOptions);
+	}
+
+	private void setupCFRMenu(JMenuBar menuBar) {
+		JMenu mnCfr = new JMenu("CFR");
+		// Iterate CFR options, create a checkbox for each option.
+		for (final CFRSetting setting : CFRSetting.values()) {
+			final JCheckBox chk = new JCheckBox(setting.getText());
+			chk.setSelected(setting.isEnabled());
+			chk.addActionListener(new ActionListener() {
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					setting.setEnabled(chk.isSelected());
+				}
+			});
+			mnCfr.add(chk);
+		}
+		// Add to menu bar.
+		menuBar.add(mnCfr);
+	}
+
+	private void setupFrame() {
+		frame.setTitle("JRemapper");
+		frame.setBounds(100, 100, 920, 578);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+	private void setupFileTree() {
+		pnlFileTree = new FileTree(callback);
+	}
+
+	private void setupSearchPanel() {
+		pnlSearch = new SearchPanel(callback);
+	}
+
+	private void setupSourceTabs() {
+		pnlJavaTabs = new JavaTabs();
+		// Keep a history of which classes were opened.
+		pnlJavaTabs.addChangeListener(new ChangeListener() {
+			@Override
+			public void stateChanged(ChangeEvent e) {
+				int i = pnlJavaTabs.getSelectedIndex();
+				if (i < 0) {
+					return;
+				}
+				String title = pnlJavaTabs.getTitleAt(i);
+				callback.getHistory().onSelectClass(title);
+			}
+		});
+
+		// Add ability to middle-click tabs to close them.
+		pnlJavaTabs.addMouseListener(new ActionCloseTab(pnlJavaTabs));
+	}
+
+	private void setupSplitView(NSplitPane nsplit) {
+		frame.getContentPane().add(nsplit, BorderLayout.CENTER);
+		nsplit.addNComponent(0, 200, pnlFileTree);
+		nsplit.addNComponent(2, 200, pnlSearch);
+		nsplit.addNComponent(1, 600, pnlJavaTabs);
+		// Finish split-pane porportions
+		nsplit.setNDivider(0, 150);
+		nsplit.setNDivider(1, 600);
 	}
 
 	/**
@@ -285,7 +271,7 @@ public class MainWindow {
 	 */
 	public void openTab(String title, String text) {
 		JTextArea textArea = new JTextArea(text);
-		this.pnlTabbedSources.addTab(title, textArea);
+		this.pnlJavaTabs.addTab(title, textArea);
 	}
 
 	/**
@@ -300,14 +286,11 @@ public class MainWindow {
 		// Try to get existing text area:
 		/// - If it does not exist, create a new tab.
 		/// - If it does exist, update content and set it as the current tab.
-		JavaTextArea javaArea = tabToSource.get(title);
+		JavaTextArea javaArea = pnlJavaTabs.getSourceArea(title);
 		if (javaArea == null) {
-			int index = pnlTabbedSources.getTabCount();
 			javaArea = new JavaTextArea(callback);
 			javaArea.setText(text);
-			pnlTabbedSources.addTab(title, javaArea);
-			tabToSource.put(title, javaArea);
-			pnlTabbedSources.setSelectedIndex(index);
+			pnlJavaTabs.addTab(title, javaArea);
 		} else {
 			// Re-decompile if option for refreshing is active
 			if (callback.getOptions().get(Options.REFRESH_ON_SELECT)) {
@@ -329,18 +312,7 @@ public class MainWindow {
 	 * @return true if success, false if failure.
 	 */
 	private boolean selectTab(String title) {
-		int index = -1;
-		for (int i = 0; i < pnlTabbedSources.getTabCount(); i++) {
-			if (pnlTabbedSources.getTitleAt(i).equals(title)) {
-				index = i;
-				break;
-			}
-		}
-		if (index == -1) {
-			return false;
-		}
-		pnlTabbedSources.setSelectedIndex(index);
-		return true;
+		return pnlJavaTabs.selectTab(title);
 	}
 
 	/**
@@ -350,26 +322,22 @@ public class MainWindow {
 	 *            Name of the tab to close.
 	 * @return true if success, false if failure.
 	 */
-	public boolean closeTab(String title) {
-		int index = -1;
-		for (int i = 0; i < pnlTabbedSources.getTabCount(); i++) {
-			if (pnlTabbedSources.getTitleAt(i).equals(title)) {
-				index = i;
-				break;
-			}
-		}
-		if (index == -1) {
-			return false;
-		}
-		pnlTabbedSources.remove(index);
-		return true;
+	public boolean removeTab(String title) {
+		return pnlJavaTabs.removeTab(title);
+
 	}
 
-	public void setTitle(String string) {
-		if (string == null) {
+	/**
+	 * Sets the window's title to the given text.
+	 * 
+	 * @param text
+	 *            Text to append.
+	 */
+	public void setTitle(String text) {
+		if (text == null) {
 			frame.setTitle("JRemapper");
 		} else {
-			frame.setTitle("JRemapper: " + string);
+			frame.setTitle("JRemapper: " + text);
 		}
 	}
 
@@ -380,10 +348,20 @@ public class MainWindow {
 		frame.setVisible(true);
 	}
 
+	/**
+	 * Return the FileTree.
+	 * 
+	 * @return
+	 */
 	public FileTree getFileTree() {
 		return pnlFileTree;
 	}
 
+	/**
+	 * Return the currently displayed JavaTextArea.
+	 * 
+	 * @return
+	 */
 	public JavaTextArea getSourceArea() {
 		return currentSource;
 	}
